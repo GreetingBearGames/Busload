52c089af9d2a7d0dfccb971629ca495e8dc894cb
melihsapmazz <melih221@gmail.com>
2022-12-07 20:48:14 +0300

    car-controller

--- Assets/_Scripts/CarController1.cs
+++ Assets/_Scripts/CarController1.cs
@@ -1,13 +1,12 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
-public class CarController1 : MonoBehaviour
-{
-    [SerializeField] private float fwdSpeed = 30f, horizontalSpeedFactor = 5f, inputSensitivity = 20f, lerpValue = 0.5f;
-    [SerializeField] private float rollRotSpeed = 5f, rollTurnSpeed = 15f;
+public class CarController1 : MonoBehaviour {
+    [SerializeField] private float fwdSpeed, horizontalSpeedFactor, inputSensitivity, lerpValue;
+    [SerializeField] private float rollRotSpeed, rollTurnSpeed, touchThreshold;
     [SerializeField] private GameObject floorObject;
-    [SerializeField] private GameObject finishline;
     private Touch touch;
     private Vector2 touchPos, previousTouchPos;
     private float normalizedDeltaPosition, targetPos, finishLinePos;
@@ -16,8 +15,7 @@ public class CarController1 : MonoBehaviour
     private float floorMinX, floorMaxX, planeMinX, planeMaxX;
 
 
-    void Start()
-    {
+    void Start() {
         planeObj = this.gameObject;
         planeMinX = planeObj.GetComponent<MeshCollider>().bounds.min.x;
         planeMaxX = planeObj.GetComponent<MeshCollider>().bounds.max.x;
@@ -25,42 +23,36 @@ public class CarController1 : MonoBehaviour
         floorMaxX = floorObject.GetComponent<MeshCollider>().bounds.max.x;
     }
 
-    void Update()
-    {
+    void Update() {
         TouchInput();
         MovewithSlide();
         RollRotation();
     }
 
 
-    private void TouchInput()
-    {
-        if (Input.touchCount > 0)
-        {
+    private void TouchInput() {
+        if (Input.touchCount > 0) {
             touch = Input.GetTouch(0);
 
-            if (touch.phase == TouchPhase.Began)
-            {
+            if (touch.phase == TouchPhase.Began) {
                 previousTouchPos = touch.position;
                 touchPos = touch.position;
             }
-            if (touch.phase == TouchPhase.Moved)
-            {
+            if (touch.phase == TouchPhase.Moved) {
                 touchPos = touch.position;
             }
 
             normalizedDeltaPosition = ((touchPos.x - previousTouchPos.x) / Screen.width) * inputSensitivity;
+        } else {
+            transform.rotation = Quaternion.Lerp(transform.rotation, Quaternion.Euler(transform.eulerAngles.x, transform.eulerAngles.y, 0), Time.deltaTime * rollRotSpeed * rollTurnSpeed);
+            //transform.rotation = Quaternion.Euler(transform.eulerAngles.x, transform.eulerAngles.y, 0);
         }
         targetPos = targetPos + normalizedDeltaPosition;
         targetPos = Mathf.Clamp(targetPos, floorMinX - planeMinX, floorMaxX - planeMaxX);
-
         previousTouchPos = touchPos;
     }
 
-
-
-    private void MovewithSlide()
-    {
+    private void MovewithSlide() {
         float horizontalSpeed = fwdSpeed * Time.deltaTime * horizontalSpeedFactor;
         float newPositionTarget = Mathf.Lerp(transform.position.x, targetPos, horizontalSpeed);
         float newPositionDifference = newPositionTarget - transform.position.x;
@@ -72,24 +64,17 @@ public class CarController1 : MonoBehaviour
 
 
 
-    private void RollRotation()
-    {
+    private void RollRotation() {
 
         if (transform.position.x - lastPosition.x > 0.005)  //sağa yatıyor
         {
             var desiredRotQ = Quaternion.Euler(transform.eulerAngles.x, transform.eulerAngles.y, -10);
             transform.rotation = Quaternion.Lerp(transform.rotation, desiredRotQ, Time.deltaTime * rollRotSpeed);
-        }
-        else if (transform.position.x - lastPosition.x < -0.005)  //sola yatıyor
-        {
+        } else if (transform.position.x - lastPosition.x < -0.005)  //sola yatıyor
+          {
             var desiredRotQ = Quaternion.Euler(transform.eulerAngles.x, transform.eulerAngles.y, 10);
             transform.rotation = Quaternion.Lerp(transform.rotation, desiredRotQ, Time.deltaTime * rollRotSpeed);
         }
-        if (Mathf.Abs(transform.position.x - lastPosition.x) < 0.003)  //uçak roll hareketini topluyor
-        {
-            transform.rotation = Quaternion.Lerp(transform.rotation, Quaternion.Euler(transform.eulerAngles.x, transform.eulerAngles.y, 0), Time.deltaTime * rollRotSpeed * rollTurnSpeed);
-        }
-
         lastPosition = transform.position;
     }
 }
