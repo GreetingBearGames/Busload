d80636552617e7bb151f3d24219ec4d66bd5bfe1
melihsapmazz <melih221@gmail.com>
2022-12-15 19:23:35 +0300

    Lose passenger animation added when bus hit to an obstacle.

--- Assets/_Scripts/PassengerLose.cs
+++ Assets/_Scripts/PassengerLose.cs
@@ -4,16 +4,36 @@ using UnityEngine;
 
 public class PassengerLose : MonoBehaviour {
     [SerializeField] private GameObject passenger;
-    private float _busBoundsY, _powerOfThrow = 10.0f, _radius;
+    public bool isThrow = false;
+    public int passangerToThrow = 0;
+    private float _busBoundsZ, _powerOfThrow = 10.0f, _radius = 5.0f, _upforce=1.0f;
 
     private void Start() {
-        _busBoundsY = GetComponent<Renderer>().bounds.size.y;
+        _busBoundsZ = GetComponent<Renderer>().bounds.size.z;
     }
 
     private void InstantiateAndThrowPassengers(int passengerToLose) {
         for (int i = 0; i < passengerToLose; i++) {
-            Instantiate(passenger, new Vector3(transform.position.x, Random.Range(-_busBoundsY, _busBoundsY), transform.position.z), Quaternion.identity);
+            Instantiate(passenger, new Vector3(transform.position.x, transform.position.y, transform.position.z + Random.Range(-_busBoundsZ/2, _busBoundsZ/2)), Quaternion.identity);
+        }
+    }
+    private void FixedUpdate() {
+        if(isThrow){
+            InstantiateAndThrowPassengers(passangerToThrow);
+            Detonate();
+            isThrow = false;
+        }
+    }
 
+    private void Detonate(){
+        Vector3 explosionPos = transform.position;
+        Collider[] colliders = Physics.OverlapSphere(explosionPos, _radius);
+        foreach(Collider hit in colliders){
+            Rigidbody rb = hit.GetComponent<Rigidbody>();
+            if(rb != null){
+                rb.AddExplosionForce(_powerOfThrow, explosionPos, _radius, _upforce, ForceMode.Impulse);
+            }
+            
         }
     }
 }
