76a5e5c0e6c806c03e90d27b7596c54555871534
melihsapmazz <melih221@gmail.com>
2022-12-17 01:43:56 +0300

    Bus Controller optimization.

--- Assets/_Scripts/BusController.cs
+++ Assets/_Scripts/BusController.cs
@@ -2,21 +2,24 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class BusController : MonoBehaviour
-{
+public class BusController : MonoBehaviour {
     [SerializeField] private float forwardSpeed, touchThreshold, horizontalSpeed, horizontalMoveMultiplier, rotateSpeed, rotateBackToSpeed;
-    [SerializeField] private GameObject groundObj;
+    [SerializeField] private GameObject groundObj, finishLine;
     [SerializeField] private BusProps busProps = null;
 
-    private float _deltaPosX, _groundBoundsX, _busBoundsX, _humanCountinScene;
-    private bool _isEnd = false, _isContinue = true;
-    private Vector3 _horizontalMove;
+    private float _deltaPosX, _groundBoundsX, _busBoundsX, _humanCountinScene, f = 0, _busBoundsZ;
+    private bool _isEnd = false, _isContinue = true, _isFinish = false;
+    private Vector3 _horizontalMove, afterFinishMove, startPos;
     private Touch _touch;
+    private int frames = 0, maxFrames = 180;
+    public AnimationCurve curve;
 
-    private void Start()
-    {
+    private void Start() {
+        var chassis = GameObject.FindGameObjectWithTag("Chassis");
         _groundBoundsX = groundObj.GetComponent<Renderer>().bounds.size.x;
-        _busBoundsX = GetComponent<Renderer>().bounds.size.x;
+        _busBoundsX = chassis.GetComponent<Renderer>().bounds.size.x;
+        //_busBoundsZ = chassis.GetComponent<Renderer>().bounds.size.z - chassis.transform.localPosition.z;
+        _busBoundsZ = GetComponent<Collider>().bounds.size.z;
         forwardSpeed = busProps.forwardSpeed;
         touchThreshold = busProps.touchThreshold;
         horizontalSpeed = busProps.horizontalSpeed;
@@ -24,41 +27,33 @@ public class BusController : MonoBehaviour
         rotateSpeed = busProps.rotateSpeed;
         rotateBackToSpeed = busProps.rotateBackToSpeed;
         _humanCountinScene = GameObject.FindGameObjectsWithTag("Human").Length;
+        finishLine = GameObject.FindGameObjectWithTag("FinishLine");
     }
-    private void Update()
-    {
+    private void Update() {
         MoveForward();
         MoveLeftAndRight();
         WinLevel();
         LoseLevel();
     }
-    private void MoveForward()
-    {
-        if (GameManager.Instance.IsGameStarted)
-        {
+    private void MoveForward() {
+        if (GameManager.Instance.IsGameStarted) {
             transform.position = Vector3.MoveTowards(transform.position, transform.position + transform.forward, forwardSpeed * Time.deltaTime);
         }
     }
-    private void MoveLeftAndRight()
-    {
-        if (Input.touchCount > 0)
-        {
+    private void MoveLeftAndRight() {
+        if (Input.touchCount > 0) {
             _touch = Input.GetTouch(0);
-            switch (_touch.phase)
-            {
+            switch (_touch.phase) {
                 case TouchPhase.Moved:
                     _deltaPosX = _touch.deltaPosition.x;
-                    if (_deltaPosX > touchThreshold)
-                    {     //Move to right
+                    if (_deltaPosX > touchThreshold) {     //Move to right
                         _horizontalMove = new Vector3(transform.position.x + _deltaPosX / Screen.width * horizontalMoveMultiplier, transform.position.y, transform.position.z);
                         transform.position = Vector3.Lerp(transform.position, _horizontalMove, horizontalSpeed);
                         var limitX = transform.position;
                         limitX.x = Mathf.Clamp(transform.position.x, -_groundBoundsX / 2 + _busBoundsX / 2, _groundBoundsX / 2 - _busBoundsX / 2);
                         transform.position = limitX;
                         transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.Euler(transform.eulerAngles.x, transform.eulerAngles.y, -25), rotateSpeed);
-                    }
-                    else if (_deltaPosX < -touchThreshold)
-                    {
+                    } else if (_deltaPosX < -touchThreshold) {
                         _horizontalMove = new Vector3(transform.position.x + _deltaPosX / Screen.width * horizontalMoveMultiplier, transform.position.y, transform.position.z);
                         transform.position = Vector3.Lerp(transform.position, _horizontalMove, horizontalSpeed);
                         var limitX = transform.position;
@@ -66,15 +61,13 @@ public class BusController : MonoBehaviour
                         transform.position = limitX;
                         transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.Euler(transform.eulerAngles.x, transform.eulerAngles.y, 25), rotateSpeed);
                     }
-                break;
+                    break;
             }
         }
         transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.Euler(transform.eulerAngles.x, transform.eulerAngles.y, 0), rotateBackToSpeed);
     }
-    private void WinLevel()
-    {
-        if (GameManager.Instance.isWin())
-        {
+    private void WinLevel() {
+        if (GameManager.Instance.isWin()) {
             touchThreshold = 0;
             horizontalSpeed = 0;
             horizontalMoveMultiplier = 0;
@@ -86,35 +79,34 @@ public class BusController : MonoBehaviour
             decreaseRate *= 20;
             if (decreaseRate == 0) decreaseRate = 1;
             var targetPos = GameObject.FindGameObjectsWithTag("FinishMultiplier")[(int)decreaseRate - 1].transform.position;
-            targetPos = new Vector3(transform.position.x, transform.position.y, targetPos.z);
+            targetPos = new Vector3(targetPos.x, transform.position.y, targetPos.z);
             GameManager.Instance.FinishMultiplier = (int)decreaseRate;
-
             forwardSpeed = 0;
-            transform.position = Vector3.Lerp(transform.position, targetPos, 0.01f);
-
-            if (_isContinue && Mathf.Abs(transform.position.z - targetPos.z) < 0.05f)
-            {
+            if(!_isFinish){
+                startPos = transform.position;
+                _isFinish = true;
+            }
+            var coroutine = FinishMove(startPos, targetPos);
+            StartCoroutine(coroutine);
+            if (_isContinue && transform.position.z == targetPos.z) {
                 _isEnd = true;
                 _isContinue = false;
+                //StopCoroutine(coroutine);
             }
 
         }
-        if (_isEnd && !GameManager.Instance.IsLose)
-        {
+        if (_isEnd && !GameManager.Instance.IsLose) {
             _isEnd = false;
             GameManager.Instance.UpdateMoney(GameManager.Instance.FinishMultiplier * GameManager.Instance.Money - GameManager.Instance.Money);
-            StartCoroutine(NextLevel(1.0f));
+            StartCoroutine(NextLevel(3.0f));
         }
     }
-    private IEnumerator NextLevel(float time)
-    {
+    private IEnumerator NextLevel(float time) {
         yield return new WaitForSeconds(time);
         GameManager.Instance.NextLevel();
     }
-    private void LoseLevel()
-    {
-        if (GameManager.Instance.IsLose)
-        {
+    private void LoseLevel() {
+        if (GameManager.Instance.IsLose) {
             touchThreshold = 0;
             horizontalSpeed = 0;
             horizontalMoveMultiplier = 0;
@@ -125,8 +117,20 @@ public class BusController : MonoBehaviour
         }
     }
 
-    public void StartBus()
-    {
+    public void StartBus() {
         GameManager.Instance.IsGameStarted = true;
+    }
+    private IEnumerator FinishMove(Vector3 StartPos, Vector3 EndPos) {
+        //timer += Time.deltaTime;
+        //transform.position = Vector3.Lerp(StartPos, EndPos, );
+        //Debug.Log("Transform Pos : " + transform.position);
+        float duration = 500;
+        float speed = 1 / duration;
+        while (f < 1) {
+            transform.position = Vector3.Lerp(StartPos, EndPos, curve.Evaluate(f));
+            Debug.Log(transform.position);
+            f += Time.deltaTime * speed;
+            yield return 0;
+        }
     }
 }
